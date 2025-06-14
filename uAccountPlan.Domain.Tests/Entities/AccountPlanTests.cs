700b461f490c4458cad58cd348d008dad40f419b
paulomarchon <paulomarchon@gmail.com>
2025-06-14 01:23:00 -0300

    test: Implementation of domain layer tests

---
+++ uAccountPlan.Domain.Tests/Entities/AccountPlanTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Principal;
+using Microsoft.Win32;
+using uAccountPlan.Domain.Entities;
+
+namespace uAccountPlan.Domain.Tests.Entities
+{
+    public class AccountPlanTests
+    {   
+
+        [Fact]
+        public void AccountPlan_ShouldBeCreatedSuccessfully_WithValidDataAndNullParent()
+        {
+            // Arrange
+            var expectedId = Guid.NewGuid();
+            var expectedCode = "1.0";
+            var expectedName = "Receita Pai";
+            var expectedType = AccountPlanType.REVENUE;
+            var expectedAcceptsLaunches = false;
+
+            // Act
+            var accountPlan = new AccountPlan
+            {
+                Id = expectedId,
+                Code = expectedCode,
+                Name = expectedName,
+                Type = expectedType,
+                AcceptsLaunches = expectedAcceptsLaunches
+            };
+            
+            // Assert
+            Assert.NotNull(accountPlan);
+            Assert.Equal(expectedId, accountPlan.Id);
+            Assert.Equal(expectedName, accountPlan.Name);
+            Assert.Equal(expectedCode, accountPlan.Code);
+            Assert.Equal(expectedType, accountPlan.Type);
+            Assert.Equal(expectedAcceptsLaunches, accountPlan.AcceptsLaunches);
+            Assert.Null(accountPlan.ParentId); 
+        }
+
+        [Fact]
+        public void AccountPlan_ShouldBeCreatedSuccessfully_WithValidDataAndParentId()
+        {
+            // Arrange
+            var expectedId = Guid.NewGuid();
+            var expectedCode = "1.1";
+            var expectedName = "Receita Filha";
+            var expectedType = AccountPlanType.REVENUE;
+            var expectedAcceptsLaunches = true;
+            var expectedParentId = Guid.NewGuid();
+
+            // Act
+            var accountPlan = new AccountPlan
+            {
+                Id = expectedId,
+                Code = expectedCode,
+                Name = expectedName,
+                Type = expectedType,
+                AcceptsLaunches = expectedAcceptsLaunches,
+                ParentId = expectedParentId
+            };
+
+            // Assert
+            Assert.NotNull(accountPlan);
+            Assert.Equal(expectedId, accountPlan.Id);
+            Assert.Equal(expectedName, accountPlan.Name);
+            Assert.Equal(expectedCode, accountPlan.Code);
+            Assert.Equal(expectedType, accountPlan.Type);
+            Assert.Equal(expectedAcceptsLaunches, accountPlan.AcceptsLaunches);
+            Assert.Equal(expectedParentId, accountPlan.ParentId);
+        }
+    }
+}
---
+++ uAccountPlan.Domain.Tests/Services/AccountPlanServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Moq;
+using uAccountPlan.Domain.Entities;
+using uAccountPlan.Domain.Interfaces;
+using uAccountPlan.Domain.Services;
+
+namespace uAccountPlan.Domain.Tests.Services
+{
+    public class AccountPlanServiceTests
+    {
+
+        [Fact]
+        public async Task GetAccountPlanByIdAsync_ShouldReturnAccountPlan_WhenFound()
+        {
+            // Arrange
+            var accountPlanId = Guid.NewGuid();
+            var accountPlanParentId = Guid.NewGuid();
+            var expectedAccountPlan = new AccountPlan {
+                Id = accountPlanId,
+                Code = "1.1",
+                Name = "Despesa Filha Teste",
+                Type = AccountPlanType.EXPENSE,
+                AcceptsLaunches = true,
+                ParentId = accountPlanParentId
+            };
+
+            var mockAccountPlanRepository = new Mock<IAccountPlanRepository>();
+
+            mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(accountPlanId)).ReturnsAsync(expectedAccountPlan);
+
+            var service = new AccountPlanService(mockAccountPlanRepository.Object);
+
+            // Act 
+            var result = await service.GetAccountPlanByIdAsync(accountPlanId);
+
+            // Assert 
+            Assert.NotNull(result);
+            Assert.Equal(expectedAccountPlan.Id, result.Id);
+            Assert.Equal(expectedAccountPlan.Code, result.Code);
+            Assert.Equal(expectedAccountPlan.Name, result.Name);
+            Assert.Equal(expectedAccountPlan.Type, result.Type);
+            Assert.Equal(expectedAccountPlan.AcceptsLaunches, result.AcceptsLaunches);
+            Assert.Equal(expectedAccountPlan.ParentId, result.ParentId);
+
+            mockAccountPlanRepository.Verify(repo => repo.GetByIdAsync(accountPlanId), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAccountPlanByIdAsync_ShouldReturnNull_WhenNotFound()
+        {
+            // Arrange
+            var nonExistentId = Guid.NewGuid();
+   
+            var mockAccountPlanRepository = new Mock<IAccountPlanRepository>();
+
+            mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(nonExistentId)).ReturnsAsync((AccountPlan?)null);
+
+            var service = new AccountPlanService(mockAccountPlanRepository.Object);
+
+            // Act
+            var result = await service.GetAccountPlanByIdAsync(nonExistentId);
+
+            // Assert
+            Assert.Null(result);
+
+            mockAccountPlanRepository.Verify(repo => repo.GetByIdAsync(nonExistentId), Times.Once);
+        }
+
+    }
+}
