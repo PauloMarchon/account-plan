c5aaf3b87e208d42b8c1cbe353ac2562a87c1927
paulomarchon <paulomarchon@gmail.com>
2025-06-17 12:55:43 -0300

    test: Implementation of Domain Tests

---
+++ uAccountPlan.Domain.Tests/Entities/AccountPlanTests.cs
@@ -0,0 +1,90 @@
+﻿using uAccountPlan.Domain.Entities;
+
+namespace uAccountPlan.Domain.Tests.Entities
+{
+    public class AccountPlanTests
+    {
+
+        public static IEnumerable<object[]> InvalidParentAccountPlanData()
+        {
+            yield return new object[] { "", "Parent Account Plan", AccountPlanType.EXPENSE, false, "Code cannot be null or empty." };
+            yield return new object[] { "2.0", "", AccountPlanType.EXPENSE, false, "Name cannot be null or empty." };
+            yield return new object[] { "2.0", " ", AccountPlanType.EXPENSE, false, "Name cannot be null or empty." };
+            yield return new object[] { "2.0", "Parent Account Plan", (AccountPlanType)999, false, "Type must be either REVENUE or EXPENSE." };
+            yield return new object[] { "2.0", "Parent Account Plan", AccountPlanType.EXPENSE, true, "Parent account plan does not accept launches." };
+        }
+
+        public static IEnumerable<object[]> InvalidChildAccountPlanData()
+        {
+            yield return new object[] { "2.1", "Child Account Plan", AccountPlanType.EXPENSE, true, Guid.Empty, "ParentId cannot be empty for child accounts." };
+            yield return new object[] { "2.1", "Child Account Plan", AccountPlanType.EXPENSE, true, null, "ParentId cannot be empty for child accounts." };
+        }
+
+        [Fact]
+        public void CreateParentAccountPlan_ShouldCreateParentAccountPlanSuccessfully_WhenValidParametersAreProvided()
+        {
+            // Arrange
+            string code = "2.0";
+            string name = "Parent Account Plan";
+            var type = AccountPlanType.REVENUE;
+            bool acceptsLaunches = false;
+
+            // Act
+            var accountPlan = AccountPlan.CreateParent(code, name, type, acceptsLaunches);
+
+            // Assert
+            Assert.NotNull(accountPlan);
+            Assert.NotEqual(Guid.Empty, accountPlan.Id);
+            Assert.Equal(code, accountPlan.Code);
+            Assert.Equal(name, accountPlan.Name);
+            Assert.Equal(type, accountPlan.Type);
+            Assert.Equal(acceptsLaunches, accountPlan.AcceptsLaunches);
+            Assert.Null(accountPlan.ParentId);
+        }
+
+        [Fact]
+        public void CreateChildAccountPlan_ShouldCreateChildAccountPlanSuccessfully_WhenValidParametersAreProvided()
+        {
+            // Arrange
+            string code = "2.1";
+            string name = "Child Account Plan";
+            var type = AccountPlanType.REVENUE;
+            bool acceptsLaunches = true;
+            Guid parentId = Guid.NewGuid();
+
+            // Act
+            var accountPlan = AccountPlan.CreateChild(code, name, type, acceptsLaunches, parentId);
+
+            // Assert
+            Assert.NotNull(accountPlan);
+            Assert.NotEqual(Guid.Empty, accountPlan.Id);
+            Assert.Equal(code, accountPlan.Code);
+            Assert.Equal(name, accountPlan.Name);
+            Assert.Equal(type, accountPlan.Type);
+            Assert.Equal(acceptsLaunches, accountPlan.AcceptsLaunches);
+            Assert.Equal(parentId, accountPlan.ParentId);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidParentAccountPlanData))]
+        public void CreateParentAccountPlan_ShouldThrowArgumentException_WhenInvalidParametersAreProvided(string code, string name, AccountPlanType type, bool acceptsLaunches, string expectedMessage)
+        {
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => AccountPlan.CreateParent(code, name, type, acceptsLaunches));
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidChildAccountPlanData))]
+        public void CreateChildAccountPlan_ShouldThrowArgumentException_WhenInvalidParametersAreProvided(string code, string name, AccountPlanType type, bool acceptsLaunches, Guid? parentId, string expectedMessage)
+        {
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => AccountPlan.CreateChild(code, name, type, acceptsLaunches, parentId ?? Guid.Empty));
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+    }
+}
+
+    
+        
+    
---
+++ uAccountPlan.Domain.Tests/Services/AccountPlanServiceTests.cs
@@ -0,0 +1,200 @@
+﻿using Moq;
+using uAccountPlan.Domain.Entities;
+using uAccountPlan.Domain.Interfaces;
+using uAccountPlan.Domain.Services;
+
+namespace uAccountPlan.Domain.Tests.Services
+{
+    public class AccountPlanServiceTests
+    {
+        private readonly Mock<IAccountPlanRepository> _mockAccountPlanRepository;
+        private readonly IAccountPlanService _service;
+
+        public AccountPlanServiceTests()
+        {
+            _mockAccountPlanRepository = new Mock<IAccountPlanRepository>();
+            _service = new AccountPlanService(_mockAccountPlanRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAccountPlansAsync_ShouldReturnAllAccountPlans()
+        {
+            // Arrange
+            var parentId = Guid.NewGuid();
+
+            var accountPlans = new List<AccountPlan>
+            {
+                AccountPlan.CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false ),
+                AccountPlan.CreateParent("3.0", "Parent Account Plan", AccountPlanType.REVENUE, false ),
+                AccountPlan.CreateChild("3.1", "Child Account Plan 1", AccountPlanType.REVENUE, true, parentId),
+            };
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(accountPlans);
+            
+            // Act
+            var result = await _service.GetAllAccountPlansAsync();
+            
+            // Assert
+            Assert.Equal(3, result.Count());
+            Assert.Contains(result, ap => ap.Code == "2.0");
+            Assert.Contains(result, ap => ap.Code == "3.0");
+            Assert.Contains(result, ap => ap.Code == "3.1");
+
+            _mockAccountPlanRepository.Verify(repo => repo.GetAllAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAccountPlanByIdAsync_ShouldReturnAccountPlan_WhenExists()
+        {
+            // Arrange
+            var accountPlan = AccountPlan
+                .CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(accountPlan.Id)).ReturnsAsync(accountPlan);
+            
+            // Act
+            var result = await _service.GetAccountPlanByIdAsync(accountPlan.Id);
+            
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEqual(Guid.Empty, result.Id);
+            Assert.Equal(accountPlan.Code, result.Code);
+            Assert.Equal(accountPlan.Name, result.Name);
+            Assert.Equal(accountPlan.Type, result.Type);
+            Assert.Equal(accountPlan.AcceptsLaunches, result.AcceptsLaunches);
+
+            _mockAccountPlanRepository.Verify(repo => repo.GetByIdAsync(accountPlan.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAccountPlanByIdAsync_ShouldReturnNull_WhenNotExists()
+        {
+            // Arrange
+            var nonExistentId = Guid.NewGuid();
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(nonExistentId)).ReturnsAsync((AccountPlan?)null);
+            
+            // Act
+            var result = await _service.GetAccountPlanByIdAsync(nonExistentId);
+            
+            // Assert
+            Assert.Null(result);
+
+            _mockAccountPlanRepository.Verify(repo => repo.GetByIdAsync(nonExistentId), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddAccountPlanAsync_ShouldAddAccountPlan_WhenValid()
+        {
+            // Arrange
+            var accountPlan = AccountPlan
+                .CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<AccountPlan>());
+            
+            _mockAccountPlanRepository.Setup(repo => repo.AddAsync(It.IsAny<AccountPlan>())).Returns(Task.CompletedTask);
+
+            // Act
+            await _service.AddAccountPlanAsync(accountPlan);
+            
+            // Assert
+            _mockAccountPlanRepository.Verify(repo => repo.AddAsync(accountPlan), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddAccountPlanAsync_ShouldThrow_WhenCodeAlreadyExists()
+        {
+            // Arrange
+            var newAccountPlanCode = "2.0";
+            
+            var newAccountPlan = AccountPlan
+                .CreateParent(newAccountPlanCode, "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            var existingAccountPlan = AccountPlan
+                .CreateParent(newAccountPlanCode, "Parent Account Plan", AccountPlanType.REVENUE, false);
+
+            _mockAccountPlanRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<AccountPlan> { existingAccountPlan });
+            
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _service.AddAccountPlanAsync(newAccountPlan));
+            
+            Assert.Equal("An account plan with code already exists.", exception.Message);
+
+            _mockAccountPlanRepository.Verify(repo => repo.AddAsync(It.IsAny<AccountPlan>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task AddAccountPlanAsync_ShouldThrowArgumentException_WhenParentAccountPlanDoesNotExist()
+        {
+            // Arrange
+            var nonExistingParentId = Guid.NewGuid();
+            var childAccountPlan = AccountPlan
+                .CreateChild("3.1", "Child Account Plan", AccountPlanType.REVENUE, true, nonExistingParentId);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(nonExistingParentId)).ReturnsAsync((AccountPlan?)null);
+
+            _mockAccountPlanRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<AccountPlan>());
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _service.AddAccountPlanAsync(childAccountPlan));
+            
+            Assert.Equal("Parent account plan does not exist.", exception.Message);
+           
+            _mockAccountPlanRepository.Verify(repo => repo.AddAsync(It.IsAny<AccountPlan>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task AddAccountPlanAsync_ShouldThrowArgumentException_WhenChildTypeDoesNotMatchParentType()
+        {
+            // Arrange
+            var parentAccountPlan = AccountPlan
+                .CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            var childAccountPlan = AccountPlan
+                .CreateChild("2.1", "Child Account Plan", AccountPlanType.EXPENSE, true, parentAccountPlan.Id);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetByIdAsync(parentAccountPlan.Id)).ReturnsAsync(parentAccountPlan);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<AccountPlan>());
+            
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _service.AddAccountPlanAsync(childAccountPlan));
+            
+            Assert.Equal("Child account plan type must match parent account plan type.", exception.Message);
+            
+            _mockAccountPlanRepository.Verify(repo => repo.AddAsync(It.IsAny<AccountPlan>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task DeleteAccountPlanAsync_ShouldDeleteById_WhenExists()
+        {
+            // Arrange
+            var accountPlan = AccountPlan
+                .CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.DeleteAsync(accountPlan.Id)).Returns(Task.CompletedTask);
+            
+            // Act
+            await _service.DeleteAccountPlanAsync(accountPlan.Id);
+            
+            // Assert
+            _mockAccountPlanRepository.Verify(repo => repo.DeleteAsync(accountPlan.Id), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteAccountPlanAsync_ShouldDelete_WhenAccountPlanExists()
+        {
+            // Arrange
+            var accountPlan = AccountPlan
+                .CreateParent("2.0", "Parent Account Plan", AccountPlanType.REVENUE, false);
+            
+            _mockAccountPlanRepository.Setup(repo => repo.DeleteAsync(accountPlan)).Returns(Task.CompletedTask);
+            
+            // Act
+            await _service.DeleteAccountPlanAsync(accountPlan);
+            
+            // Assert
+            _mockAccountPlanRepository.Verify(repo => repo.DeleteAsync(accountPlan), Times.Once);
+        }
+    }
+}
